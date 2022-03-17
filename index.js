a2b6ea260a6c30f2195f35909b5ac2dcb9c8c22d
tamilselvanyes <tamilselvanyes@gmail.com>
2022-03-17 12:27:17 +0530

    organised arrangements

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
-import {
-  getAllmovies,
-  createMovies,
-  updateMovieByid,
-  deleteMovieByid,
-  getMovieByid,
-} from "./helper.js";
+import { moviesRouter } from "./routes/movies.js";
 
 const app = express();
 dotenv.config();
@@ -22,68 +16,11 @@ const MONGO_URL = process.env.MONGO_URL;
 
 export const client = await createConnection();
 
-//movies[req.params.id]
-//find will retun cursor only so we have to convert into an array using toArray() method
-app.get("/movies", async (req, res) => {
-  const movies = await getAllmovies();
-  //console.log(movies);
-  res.send(movies);
-});
-
 app.get("/", function (req, res) {
   res.send("This is my new application using the express nodemon");
 });
 
-app.get("/movies/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  const movie = await getMovieByid(id);
-  movie
-    ? res.send(movie)
-    : res.status(404).send({ message: "No such movie found" });
-});
-
-app.delete("/movies/:id", async (req, res) => {
-  const id = req.params.id;
-  console.log(id);
-  const result = await deleteMovieByid(id);
-  res.send(result);
-});
-
-//This is the class code
-
-app.put("/movies/:id", async function (request, response) {
-  console.log(request.params);
-
-  // db.movies.updateOne({id: "102"}, {$set: upadateData})
-  const { id } = request.params;
-  const updateData = request.body;
-
-  const result = await updateMovieByid(id, updateData);
-  response.send(result);
-});
-
-// app.put("/movies/:id", async (req, res) => {
-//   //   db.movies.updateOne({id:id},{$set:{rating:9}})
-
-//   const id = req.params.id;
-//   console.log(id);
-//   const updateData = request.body;
-//   const result = await client
-//     .db("b30wd")
-//     .collection("movies")
-//     .updateOne({ id: id }, { $set: updateData });
-
-//   res.send(result);
-// });
-
-app.post("/movies", async function (request, response) {
-  // db.movies.insertMany(data)
-  const data = request.body;
-  console.log(data);
-  const result = await createMovies(data);
-  response.send(result);
-});
+app.use("/movies", moviesRouter);
 
 app.listen(PORT, () => console.log(`Server started ${PORT}`));
 
---
+++ routes/movies.js
@@ -0,0 +1,69 @@
+import express from "express";
+import {
+  getAllmovies,
+  createMovies,
+  updateMovieByid,
+  deleteMovieByid,
+  getMovieByid,
+} from "../helper.js";
+
+const router = express.Router();
+export const moviesRouter = router;
+//movies[req.params.id]
+//find will retun cursor only so we have to convert into an array using toArray() method
+router.get("", async (req, res) => {
+  const movies = await getAllmovies();
+  //console.log(movies);
+  res.send(movies);
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const movie = await getMovieByid(id);
+  movie
+    ? res.send(movie)
+    : res.status(404).send({ message: "No such movie found" });
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const result = await deleteMovieByid(id);
+  res.send(result);
+});
+
+//This is the class code
+
+router.put("/:id", async function (request, response) {
+  console.log(request.params);
+
+  // db.movies.updateOne({id: "102"}, {$set: upadateData})
+  const { id } = request.params;
+  const updateData = request.body;
+
+  const result = await updateMovieByid(id, updateData);
+  response.send(result);
+});
+
+router.post("", async function (request, response) {
+  // db.movies.insertMany(data)
+  const data = request.body;
+  console.log(data);
+  const result = await createMovies(data);
+  response.send(result);
+});
+
+// app.put("/movies/:id", async (req, res) => {
+//   //   db.movies.updateOne({id:id},{$set:{rating:9}})
+
+//   const id = req.params.id;
+//   console.log(id);
+//   const updateData = request.body;
+//   const result = await client
+//     .db("b30wd")
+//     .collection("movies")
+//     .updateOne({ id: id }, { $set: updateData });
+
+//   res.send(result);
+// });
